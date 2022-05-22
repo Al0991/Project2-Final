0900fb84394eff8f34873d727ad932c3dc2ea3e4
Al0991 <alphy.alvarezhernandez@lc.cuny.edu>
2022-05-22 12:25:51 -0400

    Created Project with create react app and made base for home page

---
+++ finalproject/src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Design to be the homepage maybe the submit file idea</h1>
+        </div>
+    )
+}
+
+export default HomePage
